7bd859fc43897abe7f4e241941dda42868f6454e
UltraDev <petzmagnus@gmail.com>
2026-02-27 22:10:14 +0100

    Add AssetSourceProvider

--- Parser/src/main/kotlin/app/ultradev/hytaleuiparser/source/AssetSource.kt
+++ Parser/src/main/kotlin/app/ultradev/hytaleuiparser/source/AssetSource.kt
@@ -3,7 +3,9 @@ package app.ultradev.hytaleuiparser.source
 import java.io.InputStream
 import java.nio.file.Path
 
-interface AssetSource {
+interface AssetSource : AssetSourceProvider {
+    override val assetSource: AssetSource get() = this
+
     fun listUIFiles(): List<Path>
     fun getAsset(path: Path): InputStream?
 
---
+++ Parser/src/main/kotlin/app/ultradev/hytaleuiparser/source/AssetSourceProvider.kt
@@ -0,0 +1,5 @@
+package app.ultradev.hytaleuiparser.source
+
+interface AssetSourceProvider {
+    val assetSource: AssetSource
+}
